40ae1b92e29c158ad57789cf06ffb8c8c45958f2
Albert Tan <albert.jctan@gmail.com>
2022-03-16 11:37:07 +1100

    Clone from public repo

---
+++ README.md
@@ -0,0 +1,204 @@
+# hiROS
+
+<!-- TABLE OF CONTENTS -->
+<p align="center">
+    <a href=#introduction> Introduction </a> •
+    <a href=#gestures> Gestures </a> •
+    <a href=#hiros> hiROS </a> •
+    <a href=#dependencies> Dependencies </a> •
+    <a href=#setup> Setup </a> •
+    <a href=#usage> Usage </a>
+</p>
+
+---
+
+## Introduction
+
+We proposed a set of communicative gestures for Human Robot Interactin and an RGB Image-based Gesture Recognizer Implemented in ROS. The aim of this project is to facilitate a more intuitive Human-Robot Interaction (HRI) through gestures.
+
+First, we propose a set of commands commonly used for human-robot interaction. Next, an online user study was conducted with a total of 190 participants. This was done to investigate if there was an agreed set of gestures that people intuitively used to communicate the given commands to robots when no guidance or training were given.
+
+As we found large variations among the gestures exist between participants, we proposed a set of [gestures for the proposed commands](#gestures) to be used as a common foundation for the robot interaction. We collected ~7500 video demonstrations of the proposed gestures and trained a gesture recognition model, adapting a 3D Convoluytional Neural Netowrk (CNN) as the classifer, with a final accuracy of 84.1%. The model was capable of training successfull with a relatively small amount of training data.
+
+The model was then integrated into the ROS framework and report details for a demonstrated use case, where a person commands a robot to perform a pick and place task using the proposed set. This integrated ROS gesture recognition system is made available for use, and built with the intention to allow for new adaptions depending on robot model and use case scenarios, for novel user applications. For our demonstration, we used this system to communicate between a user and a Fetch robot as shown below.
+
+<p align="center">
+    <img src="docs/image/demonstration.png" width=600px>
+</p>
+
+
+<!-- ## Poster
+
+This repository was written for my ECSE Final Year Project.
+
+<p align='center'>
+    <img src='docs/image/poster.png' width='600px'>
+</p> -->
+
+
+## Gestures
+
+We have proposed a set of commands that may be commonly used for human-robot interaction, shown in the table below. To validate our model, we used [sense](https://github.com/quic/sense) (action recognition model) as the backbone of our system. To leverage this model, we conducted a data collection study to collect a more diversifed dataset to train a custom model. Two additional gesture of "doing nothing" and "doing something else" were also introduced. These two classes are used to identify when the user is idle or performing non-communicative gestures.
+
+<p align="center">
+    <img src="docs/image/gestures.png">
+</p>
+
+<center>
+
+| List of Proposed Commands | &nbsp; |&nbsp; |
+| :--------------- | :---------------          | :---------------       |
+| 1. Start         | 10. Point to an Object    | 18. Thumbs up          | 
+| 2. Stop          | 11. Point to an Area      | 19. Thumbs down        |
+| 3. Handwave      | 12. I will Follow You     | 20. Give me an item    |
+| 4. Resume        | 13. Follow Me             | 21. Receive an item    | 
+| 5. Pause         | 14. Watch Me              | 22. Move backwards     | 
+| 6. Agree         | 15. Watch Out             | 23. Come forward       | 
+| 7. Disagree      | 16. Speed up              | 24. Move to the left   |
+| 8. Repeat        | 17. Slow down             | 25. Move to the right  |
+| 9. Undo          | &nbsp;                    | &nbsp;                 | 
+
+</center>
+
+*(The full list of video demonstration for each gesture can be found [here](https://youtube.com/playlist?list=PL-MdrmgE0ZdsBIVYdPruDXcg29n1nCLXA))*
+
+
+## Gesture Recognition Model
+
+Total number of participants: ~190
+
+Stage 1: Participants were asked how they would gesture to a robot to best convey a given command (i.e. "How would gesture to tell a robot to stop in place?')
+
+Stage 2: Participants were shown a video demonstration of how to gesture each command, and then asked to perform each demonstrated gesture after viewing the video example.
+
+Data collected from each respective stage were trained using the [sense](https://github.com/quic/sense) action recognition model. The model accuracy and confusion matrix is shown below. The model was trained for 100 epochs, with the performance converging after the 80th epoch. The model accuracy was also validated using multi-fold cross validation, with k=5.
+
+<center>
+
+Average number of videos per gesture | Stage 1 Accuracy | Stage 2 Accuracy  |
+:---:   | :---:         | :---:         |
+~50     | 17.2 ± 3.3%   | 81.0 ± 1.3%   |
+~100    | 19.7 ± 2.6%   | 79.2 ± 2.1%   |
+~150    | 21.6 ± 2.2%   | 82.0 ± 1.9%   |
+
+
+</center>
+
+<p align="center">
+    <img src="docs/model/cfn_stage1.png" width="400px">
+    <img src="docs/model/cfn_stage2.png" width="400px">
+</p>
+
+
+## Dependencies
+
+This repository was constructed using ROS Melodic (Robot Operating System) framework and uses [sense](https://github.com/TwentyBN/sense) as the main backbone.
+
+- ROS Melodic
+- [sense-for-HRI](https://github.com/alberttjc/sense-for-HRI)
+- Python 3
+
+## Setup
+
+### **Python3 for ROS Melodic**
+
+Since this repository is built in Python3 and ROS Melodic uses Python2.7, you need to first build [opencv_vision](https://github.com/ros-perception/vision_opencv) in Python3 and extend your main workspace (i.e. catkin_ws).
+
+Once you have installed all the right dependancies for [opencv_vision](https://github.com/ros-perception/vision_opencv), you have to build your workspace. 
+
+More details can be found from this [site](https://medium.com/@beta_b0t/how-to-setup-ros-with-python-3-44a69ca36674) for reference.
+
+```
+$ mkdir -p ~/build_ws/src
+$ cd ~/build_ws/src
+$ git clone -b melodic https://github.com/ros-perception/vision_opencv.git
+$ cd ~/build_ws
+$ catkin build cv_bridge
+```
+
+Once you build it, you will need to extend it into your main working environment (in this example it is catkin_ws). You will need to do this every time you rebuild your workspace
+
+```
+$ cd ~/catkin_ws
+$ catkin_make
+$ source devel/setup.bash
+$ source ~/build_ws/devel/setup.bash --extend
+```
+
+### **Gesture Recognition Model (Sense)**
+
+Before you can use it, pleaes check out the gesture recognition system [here](https://github.com/alberttjc/sense-for-HRI).
+
+<p align="center">
+    <img src="docs/image/hri_recognition.gif" width="600px">
+</p>
+
+
+
+
+## Usage
+
+Once you have setup your environment, as shown from [setup](#setup), you can run the inference layer.
+
+Run <kbd> rqt </kbd> to view the result. 
+
+```
+# To just run the inference layer
+$ roslaunch hiROS inference.launch
+
+# To run the inference layer with [video_stream_opencv]() package
+# kinect = true (if you are using kinect)
+# camera = 0 (change camera id if you have multiple cameras)
+$ roslaunch hiROS interface.launch kinect:=false camera:=0
+
+# To just use the camera, you can run this two launch files
+$ roslaunch video_stream_opencv webcam.launch
+$ roslaunch video_stream_opencv camera.launch
+```
+
+### **Thresholds**
+
+You can change the thresholds from each gesture used in this file <kbd> scripts/thresholds.py </kbd>.
+
+
+### **Opearting with a Fetch robot**
+
+To work on the Fetch robot, you need to enable SSH to communicate with it. In this case, the master should be the Fetch and the Follower should be your device.
+
+```
+# On personal device, run
+$ export ROS_MASTER_URI=http://[Fetch robot IP]:11311/
+$ export ROS_IP=[personal device IP]
+```
+
+For my project, I used these commands below:
+```
+# On personal device
+$ export ROS_MASTER_URI=http://160.69.69.80:11311/
+$ export ROS_IP=160.69.69.129
+$ roslaunch hiROS interface.launch kinect:=false camera:=0
+
+# On Fetch
+$ ssh hrigroup@160.69.69.80
+$ roslaunch hiros_smach robot.launch
+$ rosrun hiros_smach main.py
+```
+
+## Citation
+
+Please cite these papers in your publicaiton if this repository helps your research.
+
+```
+@misc{tan2021proposed,
+      title={A Proposed Set of Communicative Gestures for Human Robot Interaction and an RGB Image-based Gesture Recognizer Implemented in ROS}, 
+      author={Jia Chuan A. Tan and Wesley P. Chan and Nicole L. Robinson and Elizabeth A. Croft and Dana Kulic},
+      year={2021},
+      eprint={2109.09908},
+      archivePrefix={arXiv},
+      primaryClass={cs.RO}
+}
+```
+
+Paper links: 
+
+- [A Proposed Set of Communicative Gestures for Human Robot Interaction and an RGB Image-based Gesture Recognizer Implementd in ROS](https://arxiv.org/abs/2109.09908)
---
+++ scripts/inference.py
@@ -0,0 +1,317 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import rospy
+import sys
+import time
+from cv_bridge import CvBridge, CvBridgeError
+
+# ROS Messages
+from sensor_msgs.msg import Image
+from hiROS.msg import Gestures
+
+# Dependncies for Sense
+import torch
+import sense.display
+from sense.downstream_tasks.nn_utils import LogisticRegression
+from sense.downstream_tasks.nn_utils import Pipe
+from sense.downstream_tasks.postprocess import PostprocessClassificationOutput
+from sense.loading import build_backbone_network
+from sense.loading import load_backbone_model_from_config
+from sense.loading import update_backbone_weights, load_backbone_weights
+from thresholds import GESTURE_THRESHOLDS, LAB2INT
+
+#Dependencies for HiROS
+from collections import Callable
+from typing import List
+from typing import Optional
+from typing import Union
+
+from sense.display import DisplayResults
+from sense.engine import InferenceEngine
+from sense.downstream_tasks.nn_utils import RealtimeNeuralNet
+from sense.downstream_tasks.postprocess import PostProcessor
+from typing import Dict
+import numpy as np
+import cv2
+
+# Dependency for Darnet_ros 
+from darknet_ros_msgs.msg import BoundingBoxes
+
+# Dependency to detect package path
+from rospkg import RosPack
+package = RosPack()
+package_path = package.get_path('hiROS')
+
+# To shutdown
+import signal
+def sigint_handler(sig, frame):
+    main._stop_inference()
+    exit(0)
+
+class hiROS():
+    def __init__(
+            self,
+            neural_network: RealtimeNeuralNet,
+            post_processors: Union[PostProcessor, List[PostProcessor]],
+            thresholds: Dict[str, float],
+            callbacks: Optional[List[Callable]] = None,
+            path_in: str = Optional[None],
+            path_out: str = Optional[None],
+            use_gpu: bool = True):
+        
+        self.inference_engine = InferenceEngine(neural_network, use_gpu=use_gpu)
+
+        if isinstance(post_processors, list):
+            self.postprocessors = post_processors
+        else:
+            self.postprocessors = [post_processors]
+        
+        self.callbacks = callbacks or []
+        self.frame_index = None
+        self.clip = None
+        self.aspect_ratio = True
+
+        # ROS Subscribers
+        self.bridge = CvBridge()
+        self.pub_image = rospy.Publisher("/hiROS/interface", Image, queue_size=10)
+        self.pub_gestures = rospy.Publisher("/hiROS/gestures", Gestures, queue_size=1)
+        self.sub_image = rospy.Subscriber(image_topic, Image, self.run_inference, queue_size=1, buff_size=2**24)
+        self._start_inference()
+
+        # Display Overlay
+        self.thresholds = thresholds
+        self._start_time = None
+        self._class_prediction = None
+        self.human_detected = None
+
+    def run_inference(self, data):
+        try:
+            self.frame = self.bridge.imgmsg_to_cv2(data, "rgb8")
+        except CvBridgeError as e:
+            rospy.logerr('Converting Image Error. '+ str(e))
+
+        _frame = self.process_image(self.frame)
+        self.frame_index += 1
+        self.clip = np.roll(self.clip, -1, 1)
+        self.clip[:, -1, :, :, :] = _frame
+
+        if self.frame_index == self.inference_engine.step_size:
+            # A new clip is ready
+            self.inference_engine.put_nowait(self.clip)
+        
+        self.frame_index = self.frame_index % self.inference_engine.step_size
+
+        # Get human detection
+        if human_detection == True:
+            self.object_detection = rospy.wait_for_message('/darknet_ros/bounding_boxes', BoundingBoxes)
+            boxes = self.object_detection.bounding_boxes
+            self.human_detected = False
+
+            for box in boxes:
+                if box.id == 0:
+                    self.human_detected = True
+            
+            
+        
+        # Get predictions
+        prediction = self.inference_engine.get_nowait()
+
+        # If human is detected or human detection is disabled, it will run 
+        # the gesture recognition model
+        # Otherwise it will feed the base input as the output
+        if human_detection == False or self.human_detected == True:
+            prediction_postprocessed = self.postprocess_prediction(prediction)
+            image = self.display_prediction(self.frame, prediction_postprocessed)
+            image_msg = self.bridge.cv2_to_imgmsg(image, "rgb8")
+        else:
+            textsize = cv2.getTextSize("No Human Detected", cv2.FONT_HERSHEY_PLAIN, 2.5, 2)[0]
+            w_middle = int((self.frame.shape[1]/2) - textsize[0])
+            h_middle = int((self.frame.shape[0] + textsize[1]) / 2)
+            cv2.putText(self.frame, "No Human Detected", (w_middle,h_middle), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255,255,255), 2, cv2.LINE_AA)
+            image_msg = self.bridge.cv2_to_imgmsg(self.frame, "rgb8")
+
+        self.pub_image.publish(image_msg)
+
+        # Apply callbacks
+        if not all(callback(prediction_postprocessed) for callback in self.callbacks):
+            print('Error')  
+    
+    def process_image(self, img):
+        #self.size = [img.shape[0], img.shape[1]]
+        self.size = (self.inference_engine.expected_frame_size[0],
+            self.inference_engine.expected_frame_size[1],)
+        #print(self.size)
+        #print(self.inference_engine.step_size)
+        if self.aspect_ratio:
+            square_size = max(img.shape[0:2])
+            pad_top = int((square_size - img.shape[0]) / 2)
+            pad_bottom = square_size - img.shape[0] - pad_top
+            pad_left = int((square_size - img.shape[1]) / 2)
+            pad_right = square_size - img.shape[1] - pad_left
+            pad_img = cv2.copyMakeBorder(img, pad_top, pad_bottom, pad_left, pad_right, cv2.BORDER_CONSTANT)
+        # size of the image is (256,256,3)
+        return cv2.resize(pad_img, self.size) #if self.size else img
+
+    def postprocess_prediction(self, prediction):
+        post_processed_data = {}
+        for post_processor in self.postprocessors:
+            post_processed_data.update(post_processor(prediction))
+        return {'prediction': prediction, **post_processed_data}
+
+    def display_prediction(self, img: np.ndarray, prediction_postprocessed: dict):
+        # Live display
+        sorted_predictions = prediction_postprocessed['sorted_predictions']
+
+        # Display Top 1 result from the inference layer
+        if debug_inference == 'true':
+            for index in range(1):
+                activity, proba = sorted_predictions[index]
+                y_pos = 20* index + 40
+            x_offset = int(self.frame.shape[1]/2 + y_pos)
+            cv2.putText(img, 'Activity: {}'.format(activity[0:50]), (10,y_pos), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2, cv2.LINE_AA)
+            cv2.putText(img, 'Proba: {}'.format("{:.2f}".format(proba)), (10 + x_offset,y_pos), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2, cv2.LINE_AA)
+
+        # Display the Top 1 result after comparing the hard-coded thresholds
+        now = self._get_current_time()
+
+        if self._class_prediction and now - self._start_time < 3.0:
+            textsize = cv2.getTextSize(self._class_prediction, cv2.FONT_HERSHEY_PLAIN, 2.5, 2)[0]
+            w_middle = int((self.frame.shape[1]/2) - textsize[0])
+            h_middle = int((self.frame.shape[0] + textsize[1]) / 2)
+            cv2.putText(img, self._class_prediction, (w_middle,h_middle), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255,255,255), 2, cv2.LINE_AA)
+        else:
+            self._class_prediction = None
+            for class_name, proba in sorted_predictions:
+                if class_name in self.thresholds and proba > self.thresholds[class_name]:
+                    textsize = cv2.getTextSize(self._class_prediction, cv2.FONT_HERSHEY_PLAIN, 2.5, 2)[0]
+                    w_middle = int((self.frame.shape[1]/2) - textsize[0])
+                    h_middle = int((self.frame.shape[0] + textsize[1]) / 2)
+                    cv2.putText(img, self._class_prediction, (w_middle,h_middle), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255,255,255), 2, cv2.LINE_AA)
+                    self._class_prediction = class_name
+                    self._start_time = now
+
+                    # Message 
+                    gesture = Gestures()
+                    gesture.action = class_name
+                    gesture.action_index = LAB2INT[class_name]
+                    gesture.prob = proba
+                    self.pub_gestures.publish(gesture)
+                    break
+            
+        return img
+        #image_msg = self.bridge.cv2_to_imgmsg(img, "rgb8")
+        #self.pub_image.publish(image_msg)
+
+    @staticmethod
+    def _get_current_time() -> float:
+        """
+        Wrapper method to get the current time.
+        Extracted for ease of testing.
+        """
+        return time.perf_counter()
+
+
+    def _start_inference(self):
+        rospy.loginfo("Starting inference")
+        self.clip = np.random.randn(
+            1,
+            self.inference_engine.step_size,
+            self.inference_engine.expected_frame_size[0],
+            self.inference_engine.expected_frame_size[1],
+            3
+        )
+        self.frame_index = 0
+        self.inference_engine.start()
+
+    def _stop_inference(self):
+        rospy.loginfo("Stopping inference")
+        self.inference_engine.stop()
+
+
+if __name__ == '__main__':
+
+    # Initialize sigint handler
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    # Initialise node 
+    rospy.loginfo('Initialise h.i.R.O.S node (human interaction Robot Operating System')
+    rospy.init_node('hiROS', anonymous=True)
+    # Debugging purposes
+    #rospy.loginfo(package_path)
+
+    # Initialise parameters 
+    image_topic     =   rospy.get_param('~image_topic',     '/kinect2/qhd/image_color_rect')
+    ckpt_fn         =   rospy.get_param('~checkpoint_file', 'fyp1106.checkpoint')
+    backbone_fn     =   rospy.get_param('~backbone_file',   'strided_inflated_efficientnet.ckpt')
+    thresholds_fn   =   rospy.get_param('~thresholds',      'thresholds.py')
+    use_gpu         =   rospy.get_param('~use_gpu',         'true')
+    title           =   rospy.get_param('~title,',           None)
+
+    # Optional changes
+    human_detection =   rospy.get_param('~human_detection',  None)
+    debug_inference =   rospy.get_param('~debug_inference',  None)
+
+    if not image_topic:
+        rospy.logerr('Parameter \'camera\' is not provided')
+        sys.exit(-1)
+
+    # Constants
+    models_dir      =   os.path.join(package_path, 'models')
+    ckpt_path       =   os.path.join(models_dir, ckpt_fn)
+    backbone_path   =   os.path.join(models_dir, backbone_fn)
+
+    # Raise error if it cannot find the model
+    if not os.path.isfile(ckpt_path):
+        raise IOError(('{:s} not found.').format(ckpt_path))
+
+    # Load backbone network accoding to config file
+    backbone_model_config = load_backbone_model_from_config(models_dir)
+    backbone_weights = load_backbone_weights(backbone_path)
+    # Load custom classifier
+    checkpoint_classifier = torch.load(ckpt_path)
+    # Update original weights in case some intermediate layers have been finetuned
+    update_backbone_weights(backbone_weights,checkpoint_classifier)
+    # Create backbone network
+    backbone_network = build_backbone_network(backbone_model_config, backbone_weights)
+
+    # Used for labels for each gesture class, stored in thresholds.py file
+    INT2LAB = {value: key for key, value in LAB2INT.items()}
+
+
+    gesture_classifier = LogisticRegression(num_in=backbone_network.feature_dim,
+                                            num_out=len(INT2LAB))
+    gesture_classifier.load_state_dict(checkpoint_classifier)
+    gesture_classifier.eval()
+
+    # Concatenate feature extractor and met converter
+    net = Pipe(backbone_network, gesture_classifier)
+
+    postprocessor = [
+        PostprocessClassificationOutput(INT2LAB, smoothing=4)
+    ]
+
+    main = hiROS(
+        neural_network=net,
+        post_processors=postprocessor,
+        thresholds=GESTURE_THRESHOLDS,
+        callbacks=[],
+        use_gpu=use_gpu
+    )
+    
+    rospy.spin()
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
